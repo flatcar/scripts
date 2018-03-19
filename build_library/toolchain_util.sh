de57beb8500a99fc9070bbeb00104bd7574403fd
Iago López Galeiras <iago@kinvolk.io>
2018-03-19 15:32:31 +0100

    Disable ARM for now
    We'll be focusing on x86_64 for now.

--- build_library/toolchain_util.sh
+++ build_library/toolchain_util.sh
@@ -39,7 +39,6 @@ declare -r \
 # Usage: get_portage_arch chost
 get_portage_arch() {
     case "$1" in
-        aarch64*)   echo arm64;;
         alpha*)     echo alpha;;
         arm*)       echo arm;;
         hppa*)      echo hppa;;
@@ -61,7 +60,6 @@ get_portage_arch() {
 # Usage: get_kernel_arch chost
 get_kernel_arch() {
     case "$1" in
-        aarch64*)   echo arm64;;
         alpha*)     echo alpha;;
         arm*)       echo arm;;
         hppa*)      echo parisc;;
