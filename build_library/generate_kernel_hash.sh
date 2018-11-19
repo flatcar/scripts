38bdb38e995d8371fee75bd023077ce0837a285d
Flatcar Buildbot <buildbot@flatcar-linux.org>
2018-11-19 21:02:03 +0100

    1967.0.0

--- build_library/generate_kernel_hash.sh
+++ build_library/generate_kernel_hash.sh
@@ -10,4 +10,4 @@ version=sys.argv[2]
 
 with open(path, "rb") as f:
     kernel = f.read()
-    print json.dumps({"9": {"binaryvalues": [{"prefix": "grub_linux", "values": [{"value": hashlib.sha1(kernel).hexdigest(), "description": "coreos-%s" % version}]}]}})
+    print json.dumps({"9": {"binaryvalues": [{"prefix": "grub_linux", "values": [{"value": hashlib.sha1(kernel).hexdigest(), "description": "flatcar-%s" % version}]}]}})
