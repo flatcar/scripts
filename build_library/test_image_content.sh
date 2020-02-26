bdaf6f66b955907751c9a43dbc4d33867cc5d2da
Flatcar Buildbot <buildbot@flatcar-linux.org>
2020-02-26 15:38:06 +0100

    2345.3.0

--- build_library/test_image_content.sh
+++ build_library/test_image_content.sh
@@ -5,6 +5,7 @@
 GLSA_WHITELIST=(
 	201412-09 # incompatible CA certificate version numbers
 	201908-14 # backported both CVE fixes
+	201904-13 # git
 	201909-01 # Perl, SDK only
 	201909-08 # backported fix
 	201911-01 # package too old to even have the affected USE flag
