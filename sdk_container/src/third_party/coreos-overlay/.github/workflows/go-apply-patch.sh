2957eb592f27c07c86d770cddbfa8de7c5a1af73
Dongsu Park <dongsu@kinvolk.io>
2020-04-09 13:53:33 +0200

    .github: split out from Go into common.sh
    To avoid duplicates, make go-apply-patch.sh use the helper functions
    in `common.sh`.

--- sdk_container/src/third_party/coreos-overlay/.github/workflows/go-apply-patch.sh
+++ sdk_container/src/third_party/coreos-overlay/.github/workflows/go-apply-patch.sh
@@ -2,42 +2,21 @@
 
 set -euo pipefail
 
-branch="go-${VERSION_NEW}"
+. .github/workflows/common.sh
 
-git -C ~/flatcar-sdk/src/scripts checkout -B "${BASE_BRANCH}" "github/${BASE_BRANCH}"
-git -C ~/flatcar-sdk/src/third_party/portage-stable checkout -B "${BASE_BRANCH}" "github/${BASE_BRANCH}"
+checkout_branches "go-${VERSION_NEW}"
 
-pushd ~/flatcar-sdk/src/third_party/coreos-overlay >/dev/null || exit
-git checkout -B "${branch}" "github/${BASE_BRANCH}"
+pushd "${SDK_OUTER_SRCDIR}/third_party/coreos-overlay" >/dev/null || exit
 
 VERSION_OLD=$(sed -n "s/^DIST go\(${GO_VERSION}.[0-9]*\).*/\1/p" dev-lang/go/Manifest | sort -ruV | head -n1)
 [[ "${VERSION_NEW}" = "${VERSION_OLD}" ]] && echo "already the latest Go, nothing to do" && exit
 
-pushd "dev-lang/go" >/dev/null || exit
-git mv $(ls -1 go-${VERSION_OLD}*.ebuild | sort -ruV | head -n1) "go-${VERSION_NEW}.ebuild"
+git mv $(ls -1 dev-lang/go/go-${VERSION_OLD}*.ebuild | sort -ruV | head -n1) "dev-lang/go/go-${VERSION_NEW}.ebuild"
+
 popd >/dev/null || exit
 
-function enter() ( cd ../../..; exec cork enter -- $@ )
+generate_patches dev-lang go Go
 
-enter ebuild "/mnt/host/source/src/third_party/coreos-overlay/dev-lang/go/go-${VERSION_NEW}.ebuild" manifest --force
-
-# We can only create the actual commit in the actual source directory, not under the SDK.
-# So create a format-patch, and apply to the actual source.
-git add dev-lang/go/go-${VERSION_NEW}* metadata
-git commit -a -m "dev-lang/go: Upgrade Go ${VERSION_OLD} to ${VERSION_NEW}"
-
-# Generate metadata after the main commit was done.
-enter /mnt/host/source/src/scripts/update_metadata --commit coreos
-
-# Create 2 patches, one for the main ebuilds, the other for metadata changes.
-git format-patch -2 HEAD
-popd || exit
-
-git config user.name 'Flatcar Buildbot'
-git config user.email 'buildbot@flatcar-linux.org'
-git reset --hard HEAD
-git fetch origin
-git checkout -B "${BASE_BRANCH}" "origin/${BASE_BRANCH}"
-git am ~/flatcar-sdk/src/third_party/coreos-overlay/0*.patch
+apply_patches
 
 echo ::set-output name=VERSION_OLD::"${VERSION_OLD}"
