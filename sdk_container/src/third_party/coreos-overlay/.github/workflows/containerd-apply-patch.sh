8ae55c6d779559678be5368509aaef7980063790
Dongsu Park <dongsu@kinvolk.io>
2020-04-21 16:40:49 +0200

    .github: schedule weekly Github actions for containerd
    To get containerd in sync with upstream, we need to schedule weekly
    Github actions. It runs on Friday every week, only for Alpha and Edge.
    Similar to those for Docker, we need to deal with torcx ebuilds as well,
    as they contain containerd versions.

---
+++ sdk_container/src/third_party/coreos-overlay/.github/workflows/containerd-apply-patch.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -euo pipefail
+
+. .github/workflows/common.sh
+
+checkout_branches "containerd-${VERSION_NEW}"
+
+pushd "${SDK_OUTER_SRCDIR}/third_party/coreos-overlay" >/dev/null || exit
+
+VERSION_OLD=$(sed -n "s/^DIST containerd-\([0-9]*.[0-9]*.[0-9]*\).*/\1/p" app-emulation/containerd/Manifest | sort -ruV | head -n1)
+[[ "${VERSION_NEW}" = "${VERSION_OLD}" ]] && echo "already the latest Docker, nothing to do" && exit
+
+DOCKER_VERSION=$(sed -n "s/^DIST docker-\([0-9]*.[0-9]*.[0-9]*\).*/\1/p" app-emulation/docker/Manifest | sort -ruV | head -n1)
+
+# we need to update not only the main ebuild file, but also its CONTAINERD_COMMIT,
+# which needs to point to COMMIT_HASH that matches with $VERSION_NEW from upstream containerd.
+containerdEbuildOldSymlink=$(ls -1 app-emulation/containerd/containerd-${VERSION_OLD}*.ebuild | sort -ruV | head -n1)
+containerdEbuildNewSymlink="app-emulation/containerd/containerd-${VERSION_NEW}.ebuild"
+containerdEbuildMain="app-emulation/containerd/containerd-9999.ebuild"
+git mv ${containerdEbuildOldSymlink} ${containerdEbuildNewSymlink}
+sed -i "s/CONTAINERD_COMMIT=\"\(.*\)\"/CONTAINERD_COMMIT=\"${COMMIT_HASH}\"/g" ${containerdEbuildMain}
+sed -i "s/v${VERSION_OLD}/v${VERSION_NEW}/g" ${containerdEbuildMain}
+
+# torcx ebuild file has a docker version with only major and minor versions, like 19.03.
+versionTorcx=${DOCKER_VERSION%.*}
+torcxEbuildFile=$(ls -1 app-torcx/docker/docker-${versionTorcx}*.ebuild | sort -ruV | head -n1)
+sed -i "s/containerd-${VERSION_OLD}/containerd-${VERSION_NEW}/g" ${torcxEbuildFile}
+
+popd >/dev/null || exit
+
+generate_patches app-emulation containerd Containerd
+
+apply_patches
+
+echo ::set-output name=VERSION_OLD::"${VERSION_OLD}"
