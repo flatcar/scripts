ee727b639ff38623af0f064c87af4a085ffe0b77
Dongsu Park <dongsu@kinvolk.io>
2020-05-13 12:05:23 +0200

    .github: generate package versions from input values
    We do not need to specify each version from each workflow yaml file.
    Make *-apply.patch scripts instead generate `$VERSION_SHORT` from the
    input version value.

--- sdk_container/src/third_party/coreos-overlay/.github/workflows/go-apply-patch.sh
+++ sdk_container/src/third_party/coreos-overlay/.github/workflows/go-apply-patch.sh
@@ -2,13 +2,16 @@
 
 set -euo pipefail
 
+# trim the 3rd part in the input semver, e.g. from 1.14.3 to 1.14
+VERSION_SHORT=${VERSION_NEW%.*}
+
 . .github/workflows/common.sh
 
 checkout_branches "go-${VERSION_NEW}"
 
 pushd "${SDK_OUTER_SRCDIR}/third_party/coreos-overlay" >/dev/null || exit
 
-VERSION_OLD=$(sed -n "s/^DIST go\(${GO_VERSION}.[0-9]*\).*/\1/p" dev-lang/go/Manifest | sort -ruV | head -n1)
+VERSION_OLD=$(sed -n "s/^DIST go\(${VERSION_SHORT}.[0-9]*\).*/\1/p" dev-lang/go/Manifest | sort -ruV | head -n1)
 [[ "${VERSION_NEW}" = "${VERSION_OLD}" ]] && echo "already the latest Go, nothing to do" && exit
 
 git mv $(ls -1 dev-lang/go/go-${VERSION_OLD}*.ebuild | sort -ruV | head -n1) "dev-lang/go/go-${VERSION_NEW}.ebuild"
--- sdk_container/src/third_party/coreos-overlay/.github/workflows/kernel-apply-patch.sh
+++ sdk_container/src/third_party/coreos-overlay/.github/workflows/kernel-apply-patch.sh
@@ -2,15 +2,18 @@
 
 set -euo pipefail
 
+# trim the 3rd part in the input semver, e.g. from 5.4.1 to 5.4
+VERSION_SHORT=${VERSION_NEW%.*}
+
 . .github/workflows/common.sh
 
 checkout_branches "linux-${VERSION_NEW}"
 
 pushd "${SDK_OUTER_SRCDIR}/third_party/coreos-overlay" >/dev/null || exit
 
-VERSION_OLD=$(sed -n "s/^DIST patch-\(${KERNEL_VERSION}.[0-9]*\).*/\1/p" sys-kernel/coreos-sources/Manifest)
+VERSION_OLD=$(sed -n "s/^DIST patch-\(${VERSION_SHORT}.[0-9]*\).*/\1/p" sys-kernel/coreos-sources/Manifest)
 if [[ -z "${VERSION_OLD}" ]]; then
-  VERSION_OLD=$(sed -n "s/^DIST linux-\(${KERNEL_VERSION}*\).*/\1/p" sys-kernel/coreos-sources/Manifest)
+  VERSION_OLD=$(sed -n "s/^DIST linux-\(${VERSION_SHORT}*\).*/\1/p" sys-kernel/coreos-sources/Manifest)
 fi
 [[ "${VERSION_NEW}" = "${VERSION_OLD}" ]] && echo "already the latest Kernel, nothing to do" && exit
 
