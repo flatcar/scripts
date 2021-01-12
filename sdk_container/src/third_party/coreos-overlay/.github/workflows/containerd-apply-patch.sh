b41e27188fdc20e8248bd7ab22206f56312afe27
Dongsu Park <dongsu@kinvolk.io>
2021-01-12 12:06:14 +0100

    .github: escape dot correctly in sed expressions
    So far all sed expressions have used correct regular expressions around
    semantic versions, around `.`. As a result, they matched strings even
    without correct dots in place.
    
    We need to escape the dot correctly.

--- sdk_container/src/third_party/coreos-overlay/.github/workflows/containerd-apply-patch.sh
+++ sdk_container/src/third_party/coreos-overlay/.github/workflows/containerd-apply-patch.sh
@@ -13,14 +13,14 @@ fi
 
 pushd "${SDK_OUTER_SRCDIR}/third_party/coreos-overlay" >/dev/null || exit
 
-VERSION_OLD=$(sed -n "s/^DIST containerd-\([0-9]*.[0-9]*.[0-9]*\).*/\1/p" app-emulation/containerd/Manifest | sort -ruV | head -n1)
+VERSION_OLD=$(sed -n "s/^DIST containerd-\([0-9]*\.[0-9]*\.[0-9]*\).*/\1/p" app-emulation/containerd/Manifest | sort -ruV | head -n1)
 if [[ "${VERSION_NEW}" = "${VERSION_OLD}" ]]; then
   echo "already the latest Containerd, nothing to do"
   UPDATE_NEEDED=0
   exit 0
 fi
 
-DOCKER_VERSION=$(sed -n "s/^DIST docker-\([0-9]*.[0-9]*.[0-9]*\).*/\1/p" app-emulation/docker/Manifest | sort -ruV | head -n1)
+DOCKER_VERSION=$(sed -n "s/^DIST docker-\([0-9]*\.[0-9]*\.[0-9]*\).*/\1/p" app-emulation/docker/Manifest | sort -ruV | head -n1)
 
 # we need to update not only the main ebuild file, but also its CONTAINERD_COMMIT,
 # which needs to point to COMMIT_HASH that matches with $VERSION_NEW from upstream containerd.
--- sdk_container/src/third_party/coreos-overlay/.github/workflows/kernel-apply-patch.sh
+++ sdk_container/src/third_party/coreos-overlay/.github/workflows/kernel-apply-patch.sh
@@ -15,7 +15,7 @@ fi
 
 pushd "${SDK_OUTER_SRCDIR}/third_party/coreos-overlay" >/dev/null || exit
 
-VERSION_OLD=$(sed -n "s/^DIST patch-\(${VERSION_SHORT}.[0-9]*\).*/\1/p" sys-kernel/coreos-sources/Manifest)
+VERSION_OLD=$(sed -n "s/^DIST patch-\(${VERSION_SHORT}\.[0-9]*\).*/\1/p" sys-kernel/coreos-sources/Manifest)
 if [[ -z "${VERSION_OLD}" ]]; then
   VERSION_OLD=$(sed -n "s/^DIST linux-\(${VERSION_SHORT}*\).*/\1/p" sys-kernel/coreos-sources/Manifest)
 fi
