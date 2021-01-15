ad8204af82b1cc3cb9fe46b6185f2a341c8faa59
Margarita Manterola <marga@kinvolk.io>
2021-01-15 17:57:11 +0100

    vm_image_util: Use new aws_pro flag for ec2-compat

--- build_library/vm_image_util.sh
+++ build_library/vm_image_util.sh
@@ -217,7 +217,7 @@ IMG_ami_vmdk_OEM_USE=ec2
 # AWS Pro
 IMG_ami_vmdk_pro_DISK_FORMAT=vmdk_stream
 IMG_ami_vmdk_pro_OEM_PACKAGE=oem-ec2-compat
-IMG_ami_vmdk_pro_OEM_USE=ec2
+IMG_ami_vmdk_pro_OEM_USE=aws_pro
 
 ## openstack, supports ec2's metadata format so use oem-ec2-compat
 IMG_openstack_DISK_FORMAT=qcow2
