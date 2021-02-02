Cougar/coreos-overlay

x11-drivers/nvidia-metadata: Handle NVIDIA Version upgrades
Move the NVIDIA variables into a seperate file inorder to handle them
when Flatcar upgrade happens

Signed-off-by: Sayan Chowdhury <sayan@kinvolk.io>

coreos-base/coreos/coreos-0.0.1.ebuild
@@ -170,6 +170,7 @@ RDEPEND="${RDEPEND}
 	sys-libs/timezone-data
 	sys-process/lsof
 	sys-process/procps
+	x11-drivers/nvidia-conf
 "
 
 # OEM specific bits that need to go in USR

flatcar/scripts

sdk_container/src/third_party/coreos-overlay/coreos-base/coreos/coreos-0.0.1.ebuild
@@ -170,6 +170,7 @@ RDEPEND="${RDEPEND}
 	sys-libs/timezone-data
 	sys-process/lsof
 	sys-process/procps
+	x11-drivers/nvidia-metadata
 "
 
 # OEM specific bits that need to go in USR