17db3f4a0a3a96da760dced6576f2244f52873e3
Kai Lüke <kai@kinvolk.io>
2021-06-23 19:11:56 +0200

    jenkins: move all inline bash scripts to flatcar-scripts
    The logic of the inline bash scripts of each job was sometimes
    separated into the flatcar-scripts/jenkins/*.sh helpers but mostly
    part of the Groovy file. This coupling had its advantages but also
    downsides when special cases needed to be added for different release
    versions. Other issues were that the inline scripts needed the
    backslash character to be escaped twice and Jenkins was not good in
    terminating the child processes when stopping a job. Having inline
    bash scripts in Groovy also mandated the use of Jenkins to build and
    release Flatcar Container Linux which hinders test builds in other CI
    platforms.
    Move the inline bash scripts fully to to the files in
    flatcar-scripts/jenkins/ and create new ones for job that didn't have
    a script there yet. Also invoke them through a systemd-run wrapper
    script which ensures that all child processes are terminated and also
    sets up /opt/bin as additional path for the static lbzcat binary.
    A workaround for bash 4 was needed to use a temporary file instead of
    the <(cmd) bash feature which caused a strange syntax error, otherwise
    the bash commands are moved as they are.

---
+++ jenkins/kola/azure.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -ex
+
+rm -rf *.tap _kola_temp*
+
+NAME="jenkins-${JOB_NAME##*/}-${BUILD_NUMBER}"
+
+if [[ "${BOARD}" == "arm64-usr" ]]; then
+  echo "Unsupported board"
+  exit 1
+fi
+
+if [[ "${KOLA_TESTS}" == "" ]]; then
+  KOLA_TESTS="*"
+fi
+
+if [[ "${AZURE_MACHINE_SIZE}" != "" ]]; then
+  AZURE_MACHINE_SIZE_OPT="--azure-size=${AZURE_MACHINE_SIZE}"
+fi
+
+# If the OFFER is empty, it should be treated as the basic offering.
+if [[ "${OFFER}" == "" ]]; then
+  OFFER="basic"
+fi
+
+if [ "${BLOB_URL}" = "" ]; then
+  exit 0
+fi
+
+# Do not expand the kola test patterns globs
+set -o noglob
+timeout --signal=SIGQUIT 20h bin/kola run \
+    --parallel="${PARALLEL}" \
+    --basename="${NAME}" \
+    --board="${BOARD}" \
+    --channel="${GROUP}" \
+    --platform=azure \
+    --offering="${OFFER}" \
+    --azure-blob-url="${BLOB_URL}" \
+    --azure-location="${LOCATION}" \
+    --azure-profile="${AZURE_CREDENTIALS}" \
+    --azure-auth="${AZURE_AUTH_CREDENTIALS}" \
+    --tapfile="${JOB_NAME##*/}.tap" \
+    --torcx-manifest=torcx_manifest.json \
+    ${AZURE_MACHINE_SIZE_OPT} \
+    ${KOLA_TESTS}
+set +o noglob
