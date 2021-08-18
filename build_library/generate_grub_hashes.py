a314348a38fa8631037a8ffb1009029f1f544456
Krzesimir Nowak <knowak@microsoft.com>
2021-08-18 10:00:57 +0200

    build_library: Fix getting integer from string
    `string.atoi` was deprecated since at least the first release of
    python2, and looks like it was dropped in python3.

--- build_library/generate_grub_hashes.py
+++ build_library/generate_grub_hashes.py
@@ -11,7 +11,7 @@ filename = sys.argv[1]
 grubdir = sys.argv[2]
 outputdir = sys.argv[3]
 version = sys.argv[4]
-bootoffset = string.atoi(subprocess.check_output(['cgpt', 'show', '-i', '2', '-b', filename])) * 512
+bootoffset = int(subprocess.check_output(['cgpt', 'show', '-i', '2', '-b', filename])) * 512
 with open(filename, "rb") as f:
     boot = f.read(440)
     f.seek(bootoffset)
