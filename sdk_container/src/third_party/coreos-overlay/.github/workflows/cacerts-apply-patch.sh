f561ec568c3fd750d27ee8303ea6e802930906e6
Dongsu Park <dpark@linux.microsoft.com>
2021-11-16 12:18:54 +0100

    .github: add Github Actions for ca-certificates
    Automatically update app-misc/ca-certificates , a derivative of
    nss https://hg.mozilla.org/projects/nss . To make things easier,
    we simply check for new releases on its Github mirror
    https://github.com/nss-dev/nss . When the new latest tag is found,
    simply bump the version of ca-certificates ebuild.

---
+++ sdk_container/src/third_party/coreos-overlay/.github/workflows/cacerts-apply-patch.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -euo pipefail
+
+UPDATE_NEEDED=1
+
+. .github/workflows/common.sh
+
+prepare_git_repo
+
+if ! checkout_branches "${VERSION_NEW}-${TARGET}"; then
+  UPDATE_NEEDED=0
+  exit 0
+fi
+
+pushd "${SDK_OUTER_SRCDIR}/third_party/coreos-overlay" >/dev/null || exit
+
+# Parse the Manifest file for already present source files and keep the latest version in the current series
+VERSION_OLD=$(sed -n "s/^DIST nss-\([0-9]*\.[0-9]*\).*$/\1/p" app-misc/ca-certificates/Manifest | sort -ruV | head -n1)
+if [[ "${VERSION_NEW}" = "${VERSION_OLD}" ]]; then
+  echo "already the latest ca-certificates, nothing to do"
+  UPDATE_NEEDED=0
+  exit 0
+fi
+
+EBUILD_FILENAME=$(get_ebuild_filename "app-misc" "ca-certificates" "${VERSION_OLD}")
+git mv "${EBUILD_FILENAME}" "app-misc/ca-certificates/ca-certificates-${VERSION_NEW}.ebuild"
+
+popd >/dev/null || exit
+
+generate_patches app-misc ca-certificates ca-certificates
+
+apply_patches
+
+echo ::set-output name=VERSION_OLD::"${VERSION_OLD}"
+echo ::set-output name=UPDATE_NEEDED::"${UPDATE_NEEDED}"
