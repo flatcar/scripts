be97158f7d94c85f5970187826ffca7ab19196d8
Jeremi Piotrowski <jpiotrowski@microsoft.com>
2022-02-24 14:38:30 +0100

    jenkins/kola/azure: forward azure private networking parameters to kola
    These allow the configuration of virtual network for the created
    instances to join, and tell kola to use the private instance IP for
    connectivity.

--- jenkins/kola/azure.sh
+++ jenkins/kola/azure.sh
@@ -44,5 +44,7 @@ timeout --signal=SIGQUIT 20h bin/kola run \
     --torcx-manifest=torcx_manifest.json \
     ${AZURE_MACHINE_SIZE_OPT} \
     ${AZURE_HYPER_V_GENERATION:+--azure-hyper-v-generation=${AZURE_HYPER_V_GENERATION}} \
+    ${AZURE_VNET_SUBNET_NAME:+--azure-vnet-subnet-name=${AZURE_VNET_SUBNET_NAME}} \
+    ${AZURE_USE_PRIVATE_IPS:+--azure-use-private-ips=${AZURE_USE_PRIVATE_IPS}} \
     ${KOLA_TESTS}
 set +o noglob
