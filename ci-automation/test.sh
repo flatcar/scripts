af3b6d80416d3b69f2a13b6c55347438f9f88d1f
Thilo Fromm <thilo@kinvolk.io>
2022-03-10 08:24:44 +0100

    ci-automation/test.sh: handle unset PARALLEL_TESTS
    Co-authored-by: Kai Lüke <pothos@users.noreply.github.com>

--- ci-automation/test.sh
+++ ci-automation/test.sh
@@ -137,7 +137,7 @@ function test_run() {
     __prepare_torcx "${arch}" "${vernum}" "${work_dir}"
 
     # Pass PARALLEL_TESTS to the container
-    if [ -n "${PARALLEL_TESTS}" ] ; then
+    if [ -n "${PARALLEL_TESTS-}" ] ; then
         echo "PARALLEL_TESTS=\"${PARALLEL_TESTS}\"" > sdk_container/.env
         echo "rm -f 'sdk_container/.env'" >> ./ci-cleanup.sh
     fi
