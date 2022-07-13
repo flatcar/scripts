b518c3cdb8b78f8501760f94a56dcd2d0ecdd576
Gabriel Adrian Samfira <gsamfira@cloudbasesolutions.com>
2022-07-13 14:19:50 +0300

    Disable image mangle in qemu tests
    Signed-off-by: Gabriel Adrian Samfira <gsamfira@cloudbasesolutions.com>

--- ci-automation/vendor-testing/qemu.sh
+++ ci-automation/vendor-testing/qemu.sh
@@ -49,6 +49,7 @@ kola run \
     --qemu-image="${QEMU_IMAGE_NAME}" \
     --tapfile="${CIA_TAPFILE}" \
     --torcx-manifest="${CIA_TORCX_MANIFEST}" \
+    --qemu-skip-mangle \
     "${@}"
 
 set +x
