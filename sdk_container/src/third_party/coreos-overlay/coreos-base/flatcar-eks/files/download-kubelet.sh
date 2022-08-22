3c3b1117467070659b362514ba68ba582e8b3805
Ehsan Assgari <ehs@lunar.app>
2022-08-22 14:36:48 +0200

    Update coreos-base/flatcar-eks/files/download-kubelet.sh
    Co-authored-by: Mathieu Tortuyaux <mathieu.tortuyaux@gmail.com>

--- sdk_container/src/third_party/coreos-overlay/coreos-base/flatcar-eks/files/download-kubelet.sh
+++ sdk_container/src/third_party/coreos-overlay/coreos-base/flatcar-eks/files/download-kubelet.sh
@@ -23,10 +23,10 @@ fi
 # https://github.com/awslabs/amazon-eks-ami/blob/master/Makefile
 case $CLUSTER_VERSION in
   1.23)
-    S3_PATH="1.23.9/2022-07-27/"
+    S3_PATH="1.23.9/2022-07-27"
     ;;
   1.22)
-    S3_PATH="1.22.12/2022-07-27/"
+    S3_PATH="1.22.12/2022-07-27"
     ;;
   1.21)
     S3_PATH="1.21.2/2021-07-05"
