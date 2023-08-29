72622a4ba16dbb34ac7bc048cf8c1a68c6f26d7f
Krzesimir Nowak <knowak@microsoft.com>
2023-08-29 12:37:59 +0200

    fix

--- pkg_auto/pkg_auto_lib.sh
+++ pkg_auto/pkg_auto_lib.sh
@@ -1064,7 +1064,7 @@ function consistency_checks() {
             else
                 continue
             fi
-            mvm_add "${pkg_slot_verminmax_mvm_var_name}" "${pkg}" "${S}" "${verminmax}"
+            mvm_add "${pkg_slot_verminmax_mvm_var_name}" "${pkg}" "${s}" "${verminmax}"
         done
         unset -n slots_set_ref slot_verminmax2_map_ref slot_verminmax1_map_ref
     done
