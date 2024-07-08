1d7d53fad99460f79570a1ecbab9b7d98873d31b
James Le Cuirot <jlecuirot@microsoft.com>
2024-07-08 17:40:52 +0100

    Upgrade to Catalyst 4
    Catalyst 4 has totally changed the way repositories are handled. It only
    works when the name of the directory containing the repository matches
    the configured name of that repository. This was not the case for us,
    with the coreos repository residing in the coreos-overlay directory. We
    wanted to move and rename our repositories anyway, but this is a big
    change, so we'll do separately. For now, this just renames coreos to
    coreos-overlay.
    
    Catalyst 4 also ingests the main repository snapshot as a squashfs
    rather than a tarball. It features a utility to generate such a
    snapshot, but it doesn't fit Flatcar well, particularly because it
    expects each ebuild repository to reside at the top level of its own git
    repository. It was very easy to call tar2sqfs manually though.
    
    Signed-off-by: James Le Cuirot <jlecuirot@microsoft.com>

--- build_library/catalyst_sdk.sh
+++ build_library/catalyst_sdk.sh
@@ -11,7 +11,7 @@ echo "Setting the default Python interpreter"
 eselect python update
 
 echo "Building cross toolchain for the SDK."
-configure_crossdev_overlay / /tmp/crossdev
+configure_crossdev_overlay / /usr/local/portage/crossdev
 
 for cross_chost in $(get_chost_list); do
     echo "Building cross toolchain for ${cross_chost}"
@@ -21,10 +21,11 @@ for cross_chost in $(get_chost_list); do
         install_cross_rust "${cross_chost}" ${clst_myemergeopts}
 done
 
-echo "Saving snapshot of coreos-overlay repo for future SDK bootstraps"
-# Copy coreos-overlay, which is in /var/gentoo/repos/local/, into a
-# local directory.  /var/gentoo/repos/local/ is removed before archiving
-# and we want to keep a snapshot. This snapshot is used - alongside
-# /var/gentoo/repos/gentoo - by stage 1 of future bootstraps.
-mkdir -p /var/gentoo/repos/coreos-overlay
-cp -R /var/gentoo/repos/local/* /var/gentoo/repos/coreos-overlay
+echo "Saving snapshot of repos for future SDK bootstraps"
+# Copy portage-stable and coreos-overlay, which are under
+# /mnt/host/source/src/third_party, into a local directory because they are
+# removed before archiving and we want to keep snapshots. These snapshots are
+# used by stage 1 in future bootstraps.
+mkdir -p /var/gentoo/repos/{gentoo,coreos-overlay}
+cp -R /mnt/host/source/src/third_party/portage-stable/* /var/gentoo/repos/gentoo/
+cp -R /mnt/host/source/src/third_party/coreos-overlay/* /var/gentoo/repos/coreos-overlay/
