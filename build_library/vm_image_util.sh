f20f614173852f77f354a203cb50f1c9b6c0b9a0
Jan Larwig <jan@larwig.com>
2024-11-04 10:58:28 +0100

    add ionoscloud support

--- build_library/vm_image_util.sh
+++ build_library/vm_image_util.sh
@@ -42,6 +42,8 @@ VALID_IMG_TYPES=(
     vmware_ova
     vmware_raw
     xen
+    ionoscloud
+    ionoscloud_raw
 )
 
 #list of oem package names, minus the oem- prefix
@@ -68,6 +70,7 @@ VALID_OEM_PACKAGES=(
     vagrant-virtualbox
     virtualbox
     vmware
+    ionoscloud
 )
 
 # Set at runtime to one of the above types
@@ -340,6 +343,20 @@ IMG_proxmoxve_DISK_LAYOUT=vm
 IMG_proxmoxve_OEM_PACKAGE=common-oem-files
 IMG_proxmoxve_OEM_USE=proxmoxve
 IMG_proxmoxve_OEM_SYSEXT=oem-proxmoxve
+
+## ionoscloud
+IMG_ionoscloud_OEM_USE=ionoscloud
+IMG_ionoscloud_OEM_PACKAGE=oem-ionoscloud
+IMG_ionoscloud_DISK_LAYOUT=ionoscloud
+IMG_ionoscloud_DISK_FORMAT=qcow2
+IMG_ionoscloud_DISK_EXTENSION=qcow2
+IMG_ionoscloud_FS_HOOK=ionoscloud
+
+## ionoscloud_raw
+IMG_ionoscloud_raw_OEM_USE=ionoscloud
+IMG_ionoscloud_raw_OEM_PACKAGE=oem-ionoscloud
+IMG_ionoscloud_raw_DISK_LAYOUT=ionoscloud
+IMG_ionoscloud_raw_FS_HOOK=ionoscloud
 
 ###########################################################
 
@@ -617,6 +634,16 @@ _run_box_fs_hook() {
     mkdir -p "${VM_TMP_DIR}/box"
     cp -R "${VM_TMP_ROOT}/oem/box/." "${VM_TMP_DIR}/box"
     sudo rm -fr "${VM_TMP_ROOT}/oem/box"
+}
+
+_run_ionoscloud_fs_hook() {
+    # Prepare root partition for IONOS Cloud legacy injection
+    # This is a workaround until the IONOS Cloud introduces a metadata server
+    sudo mount -o remount,rw "${VM_TMP_ROOT}"
+    sudo mkdir -p "${VM_TMP_ROOT}/var/lib/cloud/seed/nocloud"
+    sudo mkdir -p "${VM_TMP_ROOT}/etc/cloud"
+    sudo touch "${VM_TMP_ROOT}/etc/cloud/cloud.cfg"
+    sudo mount -o remount,ro "${VM_TMP_ROOT}"
 }
 
 # Write the vm disk image to the target directory in the proper format
