363f2321ab0bfacf90ae585f3227bd726007cefc
James Le Cuirot <jlecuirot@microsoft.com>
2024-11-22 11:29:25 +0000

    Delete (almost) all of the jenkins directory
    This has been superseded by code in the ci-automation directory.
    
    Signed-off-by: James Le Cuirot <jlecuirot@microsoft.com>

--- jenkins/kola/qemu_uefi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ex
-
-SCRIPTFOLDER="$(dirname "$(readlink -f "$0")")"
-if [[ "$NATIVE_ARM64" == true ]]; then
-  "${SCRIPTFOLDER}/qemu_uefi_arm64.sh" qemu_uefi
-else
-  "${SCRIPTFOLDER}/qemu_common.sh" qemu_uefi
-fi
