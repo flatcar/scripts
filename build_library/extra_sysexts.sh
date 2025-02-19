df64c79de1ac3520a60470a8a1badfe9c559ce54
Daniel Zatovic <daniel.zatovic@gmail.com>
2025-02-19 16:22:21 +0100

    WIP: Allow useflags for sysexts

--- build_library/extra_sysexts.sh
+++ build_library/extra_sysexts.sh
@@ -2,4 +2,6 @@ EXTRA_SYSEXTS=(
   zfs:sys-fs/zfs
   podman:app-containers/podman,net-misc/passt
   python:dev-lang/python,dev-python/pip
+  nvidia-drivers:x11-drivers/nvidia-drivers
+  nvidia-drivers-open:x11-drivers/nvidia-drivers:kernel-open
 )
