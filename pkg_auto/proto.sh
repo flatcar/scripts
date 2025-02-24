df70c9db3e566d50d8a7a1d2232a0984c58ccd51
Krzesimir Nowak <knowak@microsoft.com>
2025-02-24 19:58:34 +0100

    work work

--- pkg_auto/proto.sh
+++ pkg_auto/proto.sh
@@ -83,6 +83,7 @@ function parse_cache_file() {
     local var_name=${1}; shift
 
     declare -g -A "${var_name}=( [eapi]=0 [keywords]=EMPTY_ARRAY [iuse]=EMPTY_ARRAY [bdepend]=EMPTY_GROUP [depend]=EMPTY_GROUP [idepend]=EMPTY_GROUP [pdepend]=EMPTY_GROUP [rdepend]=EMPTY_GROUP [license]=EMPTY_GROUP [eclasses]=EMPTY_ARRAY )"
+    # shellcheck disable=SC2034 # shellcheck does not grok references
     local -n cache=${var_name}
 
     local -n pkg_eapi=cache['eapi']
@@ -94,6 +95,7 @@ function parse_cache_file() {
     local -n pkg_pdepend_group_name=cache['pdepend']
     local -n pkg_rdepend_group_name=cache['rdepend']
     local -n pkg_license_group_name=cache['license']
+    # shellcheck disable=SC2034 # shellcheck does not grok references
     local -n pkg_eclasses=cache['eclasses']
 
     local l
@@ -131,6 +133,135 @@ function parse_cache_file() {
                 ;;
         esac
     done <"${path}"
+}
+
+function diff_eapi() {
+    local -n old_ref=${1}; shift
+    local -n new_ref=${1}; shift
+
+    local old_eapi=${old_ref['eapi']}
+    local new_eapi=${new_ref['eapi']}
+
+    if [[ ${old_eapi} != "${new_eapi}" ]]; then
+        echo "EAPI changed from ${old_eapi@Q} to ${new_eapi@Q}"
+    fi
+}
+
+function diff_iuse() {
+    local -n old_ref=${1}; shift
+    local -n new_ref=${1}; shift
+
+    local old_iuse_var_name=${old_ref['iuse']}
+    local new_iuse_var_name=${new_ref['iuse']}
+
+    local -A old_map=() new_map=() removed same added
+    local iuse_var_name name
+
+    local -n iuse_array=${old_iuse_var_name}
+    for iuse_var_name in "${iuse_array[@]}"; do
+        # shellcheck disable=SC2178 # shellcheck does not grok references
+        local -n iuse=${iuse_var_name}
+        # shellcheck disable=SC2034 # shellcheck does not grok references
+        name=${iuse['n']}
+        # shellcheck disable=SC2034 # shellcheck does not grok references
+        old_set["${name}"]=${iuse['m']}
+        unset -n iuse
+    done
+    unset -n iuse_array
+
+    local -n iuse_array=${new_iuse_var_name}
+    for iuse_var_name in "${iuse_array[@]}"; do
+        # shellcheck disable=SC2178 # shellcheck does not grok references
+        local -n iuse=${iuse_var_name}
+        name=${iuse['n']}
+        # shellcheck disable=SC2034 # shellcheck does not grok references
+        new_set["${name}"]=${iuse['m']}
+        unset -n iuse
+    done
+    unset -n iuse_array
+
+    sets_split old_map new_map removed same added
+
+    local iuse
+    for iuse in "${!removed[@]}"; do
+        echo "removed IUSE flag ${iuse@Q}"
+    done
+    for iuse in "${!added[@]}"; do
+        echo "added IUSE flag ${iuse@Q}"
+    done
+    local old_mode new_mode mode_str
+    for iuse in "${!same[@]}"; do
+        old_mode=${old_map["${iuse}"]}
+        new_mode=${new_map["${iuse}"]}
+        if [[ ${old_mode} -ne ${new_mode} ]]; then
+            case ${new_mode} in
+                "${IUSE_DISABLED}")
+                    mode_str=disabled
+                    ;;
+                "${IUSE_ENABLED}")
+                    mode_str=enabled
+                    ;;
+            esac
+            echo "IUSE flag ${iuse@Q} became ${mode_str} by default"
+        fi
+    done
+}
+
+function diff_keywords() {
+    local -n old_ref=${1}; shift
+    local -n new_ref=${1}; shift
+
+    local old_kws_var_name=${old_ref['keywords']}
+    local new_kws_var_name=${new_ref['keywords']}
+
+    local -n old_kws=${old_kws_var_name}
+    local -n new_kws=${new_kws_var_name}
+
+    if [[ ${#old_kws[@]} -ne ${#new_kws[@]} ]]; then
+        fail 'keywords number should always be the same'
+    fi
+
+    local index old_name new_name old_level new_level level_str
+    for (( index=0; index < ${#old_kws[@]}; ++index )); do
+        local -n old_kw=${old_kws["${index}"]}
+        local -n new_kw=${new_kws["${index}"]}
+        old_name=${old_kw['n']}
+        new_name=${new_kw['n']}
+        if [[ ${old_name} != "${new_name}" ]]; then
+            fail 'keywords of the same name should always have the same index'
+        fi
+        old_level=${old_kw['l']}
+        new_level=${new_kw['l']}
+        if [[ ${old_level} -ne ${new_level} ]]; then
+            case ${new_level} in
+                "${KW_STABLE}")
+                    level_str='stable'
+                    ;;
+                "${KW_UNSTABLE}")
+                    level_str='unstable'
+                    ;;
+                "${KW_BROKEN}")
+                    level_str='broken'
+                    ;;
+                "${KW_UNKNOWN}")
+                    level_str='unknown'
+                    ;;
+            esac
+            echo "package became ${level_str} for ${new_name@Q}"
+        fi
+        unset -n new_kw old_kw
+    done
+}
+
+function diff_deps() {
+    :
+}
+
+function diff_cache_data() {
+    local -n old_var_name=${1}; shift
+    local -n new_var_name=${1}; shift
+
+    diff_eapi "${old_var_name}" "${new_var_name}"
 }
 
 load_pkgs "${arch_old_reports_dir}" old
@@ -184,7 +315,19 @@ for pkg in "${!picked_pkg_set[@]}"; do
         pkg_license_group_name=${data['license']}
 
         echo "EAPI: ${pkg_eapi}"
-        echo "KEYWORDS: ${pkg_keywords[*]}"
+        kws_str=''
+        if [[ ${#pkg_keywords[@]} -gt 0 ]]; then
+            for kw in "${pkg_keywords[@]}"; do
+                kw_str=''
+                kw_to_string "${kw}" kw_str
+                if [[ -n ${kw_str} ]]; then
+                    kws_str+=${kw_str}' '
+                fi
+            done
+            # remove trailing space
+            kws_str=${kws_str:0:$(( ${#kws_str} - 1 ))}
+        fi
+        echo "KEYWORDS: ${kws_str}"
         use_str=''
         if [[ ${#pkg_iuse[@]} -gt 0 ]]; then
             for u in "${pkg_iuse[@]}"; do
@@ -210,4 +353,7 @@ for pkg in "${!picked_pkg_set[@]}"; do
 
         unset -n pkg_used_eclasses pkg_iuse pkg_keywords data repo version
     done
+
+    # diff_cache_data old_data new_data
+
 done
