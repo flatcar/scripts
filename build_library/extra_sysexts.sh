4efb615a558075fa7120174196b779ee43894746
Daniel Zatovic <daniel.zatovic@gmail.com>
2025-03-25 16:46:21 +0100

    sysext: Add pre-built NVIDIA drivers

--- build_library/extra_sysexts.sh
+++ build_library/extra_sysexts.sh
@@ -2,6 +2,10 @@ EXTRA_SYSEXTS=(
   zfs:sys-fs/zfs
   podman:app-containers/podman,net-misc/passt
   python:dev-lang/python,dev-python/pip
+  nvidia-drivers-535:=x11-drivers/nvidia-drivers-535.230.02:-kernel-open
+  nvidia-drivers-535-open:=x11-drivers/nvidia-drivers-535.230.02:kernel-open
+  nvidia-drivers-550:=x11-drivers/nvidia-drivers-550.144.03:-kernel-open
+  nvidia-drivers-550-open:=x11-drivers/nvidia-drivers-550.144.03:kernel-open
 )
 
 _get_unversioned_sysext_packages_unsorted() {
