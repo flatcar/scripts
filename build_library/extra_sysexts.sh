820bc74c3bea19bfa7054bfd1938d672ffe7324a
Daniel Zatovic <daniel.zatovic@gmail.com>
2025-04-01 11:57:06 +0200

    sysext: Change the sysext seperator from : to |
    To be able to use the SLOT syntax, which uses : we need to change the
    sysext seperator to |.

--- build_library/extra_sysexts.sh
+++ build_library/extra_sysexts.sh
@@ -1,16 +1,16 @@
 EXTRA_SYSEXTS=(
-  zfs:sys-fs/zfs
-  podman:app-containers/podman,net-misc/passt
-  python:dev-lang/python,dev-python/pip
-  nvidia-drivers-535:=x11-drivers/nvidia-drivers-535.230.02:-kernel-open
-  nvidia-drivers-535-open:=x11-drivers/nvidia-drivers-535.230.02:kernel-open
-  nvidia-drivers-550:=x11-drivers/nvidia-drivers-550.144.03:-kernel-open
-  nvidia-drivers-550-open:=x11-drivers/nvidia-drivers-550.144.03:kernel-open
+  "zfs|sys-fs/zfs"
+  "podman|app-containers/podman,net-misc/passt"
+  "python|dev-lang/python,dev-python/pip"
+  "nvidia-drivers-535|=x11-drivers/nvidia-drivers-535.230.02|-kernel-open"
+  "nvidia-drivers-535-open|=x11-drivers/nvidia-drivers-535.230.02|kernel-open"
+  "nvidia-drivers-550|=x11-drivers/nvidia-drivers-550.144.03|-kernel-open"
+  "nvidia-drivers-550-open|=x11-drivers/nvidia-drivers-550.144.03|kernel-open"
 )
 
 _get_unversioned_sysext_packages_unsorted() {
   for sysext in "${EXTRA_SYSEXTS[@]}"; do
-    IFS=":" read -r _ PACKAGE_ATOMS _ <<< "$sysext"
+    IFS="|" read -r _ PACKAGE_ATOMS _ <<< "$sysext"
 
     IFS=,
     for atom in $PACKAGE_ATOMS; do
