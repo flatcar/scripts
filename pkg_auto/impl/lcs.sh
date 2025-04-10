a6bb5bffabed6afbeb492eb119d93d2223944181
Krzesimir Nowak <knowak@microsoft.com>
2025-04-10 17:57:14 +0200

    lcs
    workworkwrok

--- pkg_auto/impl/lcs.sh
+++ pkg_auto/impl/lcs.sh
@@ -5,95 +5,84 @@ __LCS_SH_INCLUDED__=x
 
 source "$(dirname "${BASH_SOURCE[0]}")/util.sh"
 
-# takes a function prefix and a name of a variable, which will contain
-# a name of a variable having a state for subsequent lcs_run function
+declare -gri LCS_X1_IDX=0 LCS_X2_IDX=1 LCS_IDX1_IDX=2 LCS_IDX2_IDX=3
+
+# Computes the longest common subsequence of two sequences and stores
+# it in the passed array. The common items stored in the array with
+# longest common subsequence are actually names of arrays, where each
+# array stores an item from the first sequence, an item from the
+# second sequence, an index of the first item in first sequence and an
+# index of the second item in the second sequence.
 #
-# it is expected that for given prefix, functions ${prefix}_eq,
-# ${prefix}_prep and ${prefix}_kill will be defined
-#
-# ${prefix}_eq takes two parameters and checks them for equality; the
+# The function optionally takes a name of the equality function. If no
+# such function is passed, the string equality is used. The function
+# should take two parameters and should check them for equality; the
 # parameters are items from the sequences passed to lcs_run; the
-# function should return 0 if the items are equal, not 0 otherwise
+# function should return 0 (bash true value) if the items are equal,
+# not 0 (bash false value) otherwise.
 #
-# ${prefix}_prep takes five parameters: item from first sequence, item
-# from second sequence, index of the first item in the first sequence,
-# index of the second item in the second sequence and a name of a
-# variable where the prepared item will be stored; the function should
-# create a prepared item which will be stored in the longest common
-# subsequence
+# Params:
 #
-# ${prefix}_kill takes multiple parameters, all of them being prepared
-# items that should be unset
-function lcs_setup() {
-    local func_prefix=${1}; shift
-    local -n lcs_var_name_ref=${1}; shift
-
-    local lcs_name=''
-    gen_varname lcs_setup lcs_name
-
-    local eq_func=${func_prefix}_eq prep_func=${func_prefix}_prep kill_func=${func_prefix}_kill
-    local func
-    for func in "${eq_func}" "${prep_func}" "${kill_func}"; do
-        if ! declare -pF "${func}" >/dev/null 2>/dev/null; then
-            fail "${func@Q} is not a function, is ${func_prefix@Q} a valid prefix?"
-        fi
-    done
-
-    declare -g -a "${lcs_name}"
-    local -n lcs=${lcs_name}
-
-    # shellcheck disable=SC2034 # it's a reference to a global
-    # variable declared just above
-    lcs=( "${eq_func}" "${prep_func}" "${kill_func}" )
-
-    # shellcheck disable=SC2034 # same as above
-    lcs_var_name_ref=${lcs_name}
-}
-
-# takes a name of a variable containing the state, a name of an array
-# variable containing the first sequence, a name of a variable
-# containing the second sequence and a name of a variable where the
-# longest common subsequence will be stored
+# 1 - a name of an array variable containing the first sequence
+# 2 - a name of an array variable containing the second sequence
+# 3 - a name of an array variable where the longest common subsequence
+#     will be stored
+# 4 - optional, a name of an equality function
+#
+# Example:
+#
+# a=( A B C D E F G )
+# b=( B C D G K )
+# c=()
+#
+# lcs_run a b c
+#
+# echo "a: ${a[*]}"
+# echo "b: ${b[*]}"
+#
+# cn=()
+# for i in "${c[@]}"; do
+#     declare -n ref=${i}
+#     n=${ref[${LCS_X1_IDX}]}
+#     unset -n ref
+#     cn+=( "${n}" )
+# done
+# echo "c: ${cn[*]}"
+# unset "${c[@]}"
 function lcs_run() {
-    local lcs_name=${1}; shift
     local seq1_name=${1}; shift
     local seq2_name=${1}; shift
     local common_name=${1}; shift
+    local eq_func=__lcs_str_eq
+    if [[ ${#} -gt 0 ]]; then
+        eq_func=${1}; shift
+    fi
 
-    local -n lcs_ref=${lcs_name}
+    if ! declare -pF "${eq_func}" >/dev/null 2>/dev/null; then
+        fail "${eq_func@Q} is not a function"
+    fi
 
-    # rt - runtime
-    local -a lr_lcs_rt=( "${lcs_ref[@]}" )
-    local lcs_rt_memo_map_name='' lcs_rt_memo_items_set_name=''
-
-    gen_varname lcs_rt_memo_map lcs_rt_memo_map_name
-    gen_varname lcs_rt_memo_items_set lcs_rt_memo_items_set_name
-
-    local -A "${lcs_rt_memo_map_name}=()" "${lcs_rt_memo_items_set_name}=()"
-
-    lr_lcs_rt+=( "${lcs_rt_memo_map_name}" "${lcs_rt_memo_items_set_name}" "${seq1_name}" "${seq2_name}" )
+    local -A lcs_memo_map=() lcs_memo_items_set=()
+    local -a lr_lcs_state=( "${eq_func}" lcs_memo_map lcs_memo_items_set "${seq1_name}" "${seq2_name}" )
 
     local -n seq1=${seq1_name} seq2=${seq2_name}
     local idx1=$(( ${#seq1[@]} - 1 )) idx2=$(( ${#seq2[@]} - 1 ))
 
-    __lcs_recurse lr_lcs_rt "${idx1}" "${idx2}"
+    __lcs_recurse lr_lcs_state "${idx1}" "${idx2}"
 
-    local -n common_ref=${common_name} items_set_ref=${lcs_rt_memo_items_set_name} memo_map_ref=${lcs_rt_memo_map_name}
-    local memo_key=''
+    local -n common_ref=${common_name}
+    local lr_memo_key=''
     __lcs_make_memo_key "${idx1}" "${idx2}" lr_memo_key
-    local -n memoized_items_ref=${memo_map_ref["${lr_memo_key}"]}
+    local -n memoized_items_ref=${lcs_memo_map["${lr_memo_key}"]}
 
     common_ref=( "${memoized_items_ref[@]}" )
 
     local item
     for item in "${common_ref[@]}"; do
-        unset "items_set_ref[${item}]"
+        unset "lcs_memo_items_set[${item}]"
     done
 
-    local kill_func=${lr_lcs_rt[${__LCS_KILL_IDX}]}
-    if [[ ${#items_set_ref[@]} -gt 0 ]]; then
-        "${kill_func}" "${!items_set_ref[@]}"
-    fi
+    unset "${!lcs_memo_items_set[@]}"
     local items_var_name memo_key
     for memo_key in "${!memo_map_ref[@]}"; do
         items_var_name=${memo_map_ref["${memo_key}"]}
@@ -103,12 +92,14 @@ function lcs_run() {
     done
 }
 
-declare -gri __LCS_EQ_IDX=0 __LCS_PREP_IDX=1 __LCS_KILL_IDX=2
-# used during lcs_run only
-declare -gri __LCS_MEMO_MAP_IDX=3 __LCS_MEMO_ITEMS_SET_IDX=4 __LCS_SEQ1_IDX=5 __LCS_SEQ2_IDX=6
+##
+## Details
+##
+
+declare -gri __LCS_EQ_IDX=0 __LCS_MEMO_MAP_IDX=1 __LCS_MEMO_ITEMS_SET_IDX=2 __LCS_SEQ1_IDX=3 __LCS_SEQ2_IDX=4
 
 function __lcs_recurse() {
-    local lcs_rt_name=${1}; shift
+    local lcs_state_name=${1}; shift
     local i1=${1}; shift
     local i2=${1}; shift
 
@@ -116,9 +107,9 @@ function __lcs_recurse() {
         return 0
     fi
 
-    local -n lcs_rt=${lcs_rt_name}
+    local -n lcs_state=${lcs_state_name}
 
-    local -n memo_map_ref=${lcs_rt[${__LCS_MEMO_MAP_IDX}]}
+    local -n memo_map_ref=${lcs_state[${__LCS_MEMO_MAP_IDX}]}
     local memo_key=''
     __lcs_make_memo_key "${i1}" "${i2}" memo_key
     local seq_name=${memo_map_ref["${memo_key}"]:-}
@@ -128,19 +119,19 @@ function __lcs_recurse() {
     unset seq_name memo_key
     unset -n memo_map_ref
 
-    local -n seq1=${lcs_rt[${__LCS_SEQ1_IDX}]}
-    local -n seq2=${lcs_rt[${__LCS_SEQ2_IDX}]}
+    local -n seq1=${lcs_state[${__LCS_SEQ1_IDX}]}
+    local -n seq2=${lcs_state[${__LCS_SEQ2_IDX}]}
     local x1=${seq1["${i1}"]}
     local x2=${seq2["${i2}"]}
     unset -n seq2 seq1
 
     local equal=x
-    local eq_func=${lcs_rt[${__LCS_EQ_IDX}]}
+    local eq_func=${lcs_state[${__LCS_EQ_IDX}]}
     "${eq_func}" "${x1}" "${x2}" || equal=''
     unset eq_func
 
     if [[ -n "${equal}" ]]; then
-        __lcs_recurse "${lcs_rt_name}" $((i1 - 1)) $((i2 - 1))
+        __lcs_recurse "${lcs_state_name}" $((i1 - 1)) $((i2 - 1))
 
         local n
         gen_varname lcs_common n
@@ -151,32 +142,35 @@ function __lcs_recurse() {
         # called above), make a copy of it, add the prepared item and
         # memoize that copy for i1 and i2; also add the prepared item
         # to the set of prepared items
-        local -n memo_map_ref=${lcs_rt[${__LCS_MEMO_MAP_IDX}]}
+        local -n memo_map_ref=${lcs_state[${__LCS_MEMO_MAP_IDX}]}
         local previous_memo_key=''
         __lcs_make_memo_key "$((i1 - 1))" "$((i2 - 1))" previous_memo_key
         local -n previous_memoized_prepared_items_ref=${memo_map_ref["${previous_memo_key}"]:-EMPTY_ARRAY}
         local -n c=${n}
-        local prepared_item_to_insert='NONE' prep_func=${lcs_rt[${__LCS_PREP_IDX}]}
-        "${prep_func}" "${x1}" "${x2}" "${i1}" "${i2}" prepared_item_to_insert
+        local prepared_item_to_insert
+
+        gen_varname prepared_item_to_insert
+        declare -g -a "${prepared_item_to_insert}=( ${x1@Q} ${x2@Q} ${i1@Q} ${i2@Q} )"
+
         c=( "${previous_memoized_prepared_items_ref[@]}" "${prepared_item_to_insert}" )
 
         local memo_key="x${i1}x${i2}x"
         __lcs_make_memo_key "${i1}" "${i2}" memo_key
         memo_map_ref["${memo_key}"]=${n}
 
-        local -n memo_items_set=${lcs_rt[${__LCS_MEMO_ITEMS_SET_IDX}]}
+        local -n memo_items_set=${lcs_state[${__LCS_MEMO_ITEMS_SET_IDX}]}
         # shellcheck disable=SC2034 # shellcheck does not grok references
         memo_items_set["${prepared_item_to_insert}"]=x
     else
-        __lcs_recurse "${lcs_rt_name}" "${i1}" $((i2 - 1))
-        __lcs_recurse "${lcs_rt_name}" $((i1 - 1)) "${i2}"
+        __lcs_recurse "${lcs_state_name}" "${i1}" $((i2 - 1))
+        __lcs_recurse "${lcs_state_name}" $((i1 - 1)) "${i2}"
 
         # retrieve memoized results for i1 and i2-1 and for i1-1 and
         # i2 (what we just called above), and memoize the longer
         # result for i1 and i2
         #
         # shellcheck disable=SC2178 # shellcheck does not grok references
-        local -n memo_map_ref=${lcs_rt[${__LCS_MEMO_MAP_IDX}]}
+        local -n memo_map_ref=${lcs_state[${__LCS_MEMO_MAP_IDX}]}
         local previous_memo_key=''
         __lcs_make_memo_key "${i1}" "$((i2 - 1))" previous_memo_key
         local previous_memoized_prepared_items_name1=${memo_map_ref["${previous_memo_key}"]:-EMPTY_ARRAY}
@@ -189,7 +183,6 @@ function __lcs_recurse() {
 
         local memo_key=''
         __lcs_make_memo_key "${i1}" "${i2}" memo_key
-        local kill_func=${lcs_rt[${__LCS_KILL_IDX}]}
         if [[ ${#previous_memoized_prepared_items_ref1[@]} -gt ${#previous_memoized_prepared_items_ref2[@]} ]]; then
             memo_map_ref["${memo_key}"]=${previous_memoized_prepared_items_name1}
         else
@@ -207,45 +200,8 @@ function __lcs_make_memo_key() {
     ref=${key}
 }
 
-##
-##
-##
-
-if false; then
-
-# takes two items, compares them, returns true if equal, false
-# otherwise
-function simple_eq() {
+function __lcs_str_eq() {
     [[ "${1}" = "${2}" ]]
 }
-
-# takes two items that compared equal, two indices (where in both
-# sequences those items are) and a name of the variable where the
-# prepared item to be stored in common array should be placed
-function simple_prep() {
-    local item=${1}; shift 4; local -n ref=${1}; shift
-    # shellcheck disable=SC2034 # shellcheck does not grok references
-    ref=${item}
-}
-
-# takes all prepared items that should be unset
-function simple_kill() {
-    :
-}
-
-a=( A B C D E F G )
-b=( B C D G K )
-c=()
-
-test_lcs_name=''
-
-lcs_setup simple test_lcs_name
-lcs_run "${test_lcs_name}" a b c
-
-echo "a: ${a[*]}"
-echo "b: ${b[*]}"
-echo "c: ${c[*]}"
-
-fi
 
 fi
