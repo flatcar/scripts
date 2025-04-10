bf1fa5ca58f3c5794a5aac6c57c85a8cf5452d6a
Krzesimir Nowak <knowak@microsoft.com>
2025-04-10 18:12:49 +0200

    lcs

--- pkg_auto/impl/lcs.sh
+++ pkg_auto/impl/lcs.sh
@@ -43,7 +43,7 @@ declare -gri LCS_X1_IDX=0 LCS_X2_IDX=1 LCS_IDX1_IDX=2 LCS_IDX2_IDX=3
 # cn=()
 # for i in "${c[@]}"; do
 #     declare -n ref=${i}
-#     n=${ref[${LCS_X1_IDX}]}
+#     n=${ref[LCS_X1_IDX]}
 #     unset -n ref
 #     cn+=( "${n}" )
 # done
@@ -66,7 +66,7 @@ function lcs_run() {
     local -a lr_lcs_state=( "${eq_func}" lcs_memo_map lcs_memo_items_set "${seq1_name}" "${seq2_name}" )
 
     local -n seq1=${seq1_name} seq2=${seq2_name}
-    local idx1=$(( ${#seq1[@]} - 1 )) idx2=$(( ${#seq2[@]} - 1 ))
+    local -i idx1=$(( ${#seq1[@]} - 1 )) idx2=$(( ${#seq2[@]} - 1 ))
 
     __lcs_recurse lr_lcs_state "${idx1}" "${idx2}"
 
@@ -100,18 +100,18 @@ declare -gri __LCS_EQ_IDX=0 __LCS_MEMO_MAP_IDX=1 __LCS_MEMO_ITEMS_SET_IDX=2 __LC
 
 function __lcs_recurse() {
     local lcs_state_name=${1}; shift
-    local i1=${1}; shift
-    local i2=${1}; shift
+    local -i i1=${1}; shift
+    local -i i2=${1}; shift
 
-    if [[ ${i1} -lt 0 || ${i2} -lt 0 ]]; then
+    if [[ i1 -lt 0 || i2 -lt 0 ]]; then
         return 0
     fi
 
     local -n lcs_state=${lcs_state_name}
 
-    local -n memo_map_ref=${lcs_state[${__LCS_MEMO_MAP_IDX}]}
+    local -n memo_map_ref=${lcs_state[__LCS_MEMO_MAP_IDX]}
     local memo_key=''
-    __lcs_make_memo_key "${i1}" "${i2}" memo_key
+    __lcs_make_memo_key ${i1} ${i2} memo_key
     local seq_name=${memo_map_ref["${memo_key}"]:-}
     if [[ -n ${seq_name} ]]; then
         return 0
@@ -119,14 +119,14 @@ function __lcs_recurse() {
     unset seq_name memo_key
     unset -n memo_map_ref
 
-    local -n seq1=${lcs_state[${__LCS_SEQ1_IDX}]}
-    local -n seq2=${lcs_state[${__LCS_SEQ2_IDX}]}
-    local x1=${seq1["${i1}"]}
-    local x2=${seq2["${i2}"]}
-    unset -n seq2 seq1
+    local -n seq1_ref=${lcs_state[__LCS_SEQ1_IDX]}
+    local -n seq2_ref=${lcs_state[__LCS_SEQ2_IDX]}
+    local x1=${seq1_ref[i1]}
+    local x2=${seq2_ref[i2]}
+    unset -n seq2_ref seq1_ref
 
     local equal=x
-    local eq_func=${lcs_state[${__LCS_EQ_IDX}]}
+    local eq_func=${lcs_state[__LCS_EQ_IDX]}
     "${eq_func}" "${x1}" "${x2}" || equal=''
     unset eq_func
 
@@ -142,9 +142,9 @@ function __lcs_recurse() {
         # called above), make a copy of it, add the prepared item and
         # memoize that copy for i1 and i2; also add the prepared item
         # to the set of prepared items
-        local -n memo_map_ref=${lcs_state[${__LCS_MEMO_MAP_IDX}]}
-        local previous_memo_key=''
-        __lcs_make_memo_key "$((i1 - 1))" "$((i2 - 1))" previous_memo_key
+        local -n memo_map_ref=${lcs_state[__LCS_MEMO_MAP_IDX]}
+        local previous_memo_key
+        __lcs_make_memo_key $((i1 - 1)) $((i2 - 1)) previous_memo_key
         local -n previous_memoized_prepared_items_ref=${memo_map_ref["${previous_memo_key}"]:-EMPTY_ARRAY}
         local -n c=${n}
         local prepared_item_to_insert
@@ -154,35 +154,35 @@ function __lcs_recurse() {
 
         c=( "${previous_memoized_prepared_items_ref[@]}" "${prepared_item_to_insert}" )
 
-        local memo_key="x${i1}x${i2}x"
-        __lcs_make_memo_key "${i1}" "${i2}" memo_key
+        local memo_key
+        __lcs_make_memo_key ${i1} ${i2} memo_key
         memo_map_ref["${memo_key}"]=${n}
 
-        local -n memo_items_set=${lcs_state[${__LCS_MEMO_ITEMS_SET_IDX}]}
+        local -n memo_items_set=${lcs_state[__LCS_MEMO_ITEMS_SET_IDX]}
         # shellcheck disable=SC2034 # shellcheck does not grok references
         memo_items_set["${prepared_item_to_insert}"]=x
     else
-        __lcs_recurse "${lcs_state_name}" "${i1}" $((i2 - 1))
-        __lcs_recurse "${lcs_state_name}" $((i1 - 1)) "${i2}"
+        __lcs_recurse "${lcs_state_name}" ${i1} $((i2 - 1))
+        __lcs_recurse "${lcs_state_name}" $((i1 - 1)) ${i2}
 
         # retrieve memoized results for i1 and i2-1 and for i1-1 and
         # i2 (what we just called above), and memoize the longer
         # result for i1 and i2
         #
         # shellcheck disable=SC2178 # shellcheck does not grok references
-        local -n memo_map_ref=${lcs_state[${__LCS_MEMO_MAP_IDX}]}
+        local -n memo_map_ref=${lcs_state[__LCS_MEMO_MAP_IDX]}
         local previous_memo_key=''
-        __lcs_make_memo_key "${i1}" "$((i2 - 1))" previous_memo_key
+        __lcs_make_memo_key ${i1} $((i2 - 1)) previous_memo_key
         local previous_memoized_prepared_items_name1=${memo_map_ref["${previous_memo_key}"]:-EMPTY_ARRAY}
         local -n previous_memoized_prepared_items_ref1=${previous_memoized_prepared_items_name1}
 
         previous_memo_key=''
-        __lcs_make_memo_key "$((i1 - 1))" "${i2}" previous_memo_key
+        __lcs_make_memo_key $((i1 - 1)) ${i2} previous_memo_key
         local previous_memoized_prepared_items_name2=${memo_map_ref["${previous_memo_key}"]:-EMPTY_ARRAY}
         local -n previous_memoized_prepared_items_ref2=${previous_memoized_prepared_items_name2}
 
         local memo_key=''
-        __lcs_make_memo_key "${i1}" "${i2}" memo_key
+        __lcs_make_memo_key ${i1} ${i2} memo_key
         if [[ ${#previous_memoized_prepared_items_ref1[@]} -gt ${#previous_memoized_prepared_items_ref2[@]} ]]; then
             memo_map_ref["${memo_key}"]=${previous_memoized_prepared_items_name1}
         else
