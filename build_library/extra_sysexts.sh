f524441a08be49a40127641b0fa126f414eeedcd
Daniel Zatovic <daniel.zatovic@gmail.com>
2025-04-23 11:35:19 +0200

    sysext: build nvidia-persistenced daemon

--- build_library/extra_sysexts.sh
+++ build_library/extra_sysexts.sh
@@ -2,12 +2,12 @@ EXTRA_SYSEXTS=(
   "zfs|sys-fs/zfs"
   "podman|app-containers/podman,net-misc/passt"
   "python|dev-lang/python,dev-python/pip"
-  "nvidia-drivers-535|x11-drivers/nvidia-drivers:0/535|-kernel-open|amd64"
-  "nvidia-drivers-535-open|x11-drivers/nvidia-drivers:0/535|kernel-open|amd64"
-  "nvidia-drivers-550|x11-drivers/nvidia-drivers:0/550|-kernel-open|amd64"
-  "nvidia-drivers-550-open|x11-drivers/nvidia-drivers:0/550|kernel-open|amd64"
-  "nvidia-drivers-570|x11-drivers/nvidia-drivers:0/570|-kernel-open|amd64"
-  "nvidia-drivers-570-open|x11-drivers/nvidia-drivers:0/570|kernel-open|amd64"
+  "nvidia-drivers-535|x11-drivers/nvidia-drivers:0/535|-kernel-open persistenced|amd64"
+  "nvidia-drivers-535-open|x11-drivers/nvidia-drivers:0/535|kernel-open persistenced|amd64"
+  "nvidia-drivers-550|x11-drivers/nvidia-drivers:0/550|-kernel-open persistenced|amd64"
+  "nvidia-drivers-550-open|x11-drivers/nvidia-drivers:0/550|kernel-open persistenced|amd64"
+  "nvidia-drivers-570|x11-drivers/nvidia-drivers:0/570|-kernel-open persistenced|amd64"
+  "nvidia-drivers-570-open|x11-drivers/nvidia-drivers:0/570|kernel-open persistenced|amd64"
 )
 
 _get_unversioned_sysext_packages_unsorted() {
