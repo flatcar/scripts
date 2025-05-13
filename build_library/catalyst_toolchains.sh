5198c6dd7399a78f52ff1d1cb3101f1de8fd21d3
Krzesimir Nowak <knowak@microsoft.com>
2025-05-13 13:46:08 +0200

    build_toolchains: Break dep loop and handle more dependencies
    Switching to a selinux profile caused more USE flags to be enabled
    (selinux, audit, caps), thus more dependencies to be pulled. More
    dependencies caused two things:
    
    - cyclic dependencies appeared
    - sys-apps/baselayout is being pulled in
    
    Cyclic dependencies need to be handled in a similar way it was done in
    build_packages, thus factor out the code doing it into a separate and
    reusable part.
    
    The dependency on baselayout needs to be handled by installing the
    package as a first thing in $ROOT, followed by a more careful way of
    copying things from $SYSROOT to $ROOT (due to split-usr differences),
    followed by installing the rest of the packages.
    
    Signed-off-by: Krzesimir Nowak <knowak@microsoft.com>

--- build_library/catalyst_toolchains.sh
+++ build_library/catalyst_toolchains.sh
@@ -3,6 +3,7 @@
 set -e
 source /tmp/chroot-functions.sh
 source /tmp/toolchain_util.sh
+source /tmp/break_dep_loop.sh
 
 # A note on packages:
 # The default PKGDIR is /usr/portage/packages
@@ -57,8 +58,53 @@ build_target_toolchain() {
         cp -at "${ROOT}"/usr "${SYSROOT}"/usr/include
     )
 
-    btt_emerge --update "${TOOLCHAIN_PKGS[@]}"
-    unset -f btt_emerge
+    local -a args_for_bdl=()
+    if [[ -n ${clst_VERBOSE} ]]; then
+        args_for_bdl+=(-v)
+    fi
+    function btt_bdl_portageq() {
+        ROOT=${ROOT} SYSROOT=${ROOT} PORTAGE_CONFIGROOT=${ROOT} portageq "${@}"
+    }
+    function btt_bdl_equery() {
+        ROOT=${ROOT} SYSROOT=${ROOT} PORTAGE_CONFIGROOT=${ROOT} equery "${@}"
+    }
+    # Breaking the following loops here:
+    #
+    # glibc[nscd] -> libcap[pam] -> sys-libs/pam -> libcrypt -> libxcrypt[system] -> glibc
+    # glibc[nscd] -> audit[python] -> python -> libcrypt -> libxcrypt[system] -> glibc
+    # glibc[selinux] -> libselinux[python] -> python -> libcrypt -> libxcrypt[system] -> glibc
+    # systemd[cryptsetup] -> cryptsetup[udev] -> libudev[systemd] -> systemd
+    # systemd[cryptsetup] -> cryptsetup -> lvm2[udev] -> libudev[systemd] -> systemd
+    # systemd[cryptsetup] -> cryptsetup -> lvm2[lvm,systemd] -> systemd
+    # systemd[cryptsetup] -> cryptsetup -> tmpfiles[systemd] -> systemd
+    # systemd[curl] -> curl -> nghttp2[systemd] -> systemd
+    #     importd requires curl, so needs to be disabled too
+    # systemd[pam] -> pam[systemd] -> systemd
+    #     not dropping pam from sys-apps/systemd, otherwise we would
+    #     need to drop pam from sys-auth/pambase
+    # systemd[tpm] -> tpm2-tss -> tmpfiles[systemd] -> systemd
+    # util-linux[audit] -> audit[python] -> python -> util-linux
+    # util-linux[cryptsetup] -> cryptsetup -> util-linux
+    # util-linux[pam] -> sys-libs/pam[audit] -> sys-process/audit[python] -> python -> util-linux
+    #     su requires pam, so needs to be disabled too
+    # util-linux[selinux] -> libselinux[python] -> python -> util-linux
+    # util-linux[systemd] -> systemd -> util-linux
+    # util-linux[udev] -> libudev[systemd] -> systemd -> util-linux
+    args_for_bdl+=(
+        sys-apps/systemd cryptsetup,curl,importd,tpm
+        sys-apps/util-linux audit,cryptsetup,pam,selinux,su,systemd,udev
+        sys-libs/glibc nscd,selinux
+        sys-libs/pam systemd
+    )
+    BDL_ROOT=${ROOT} \
+    BDL_PORTAGEQ=btt_bdl_portageq \
+    BDL_EQUERY=btt_bdl_equery \
+    BDL_EMERGE=btt_emerge \
+        break_dep_loop "${args_for_bdl[@]}"
+    unset btt_bdl_portageq btt_bdl_equery
+
+    btt_emerge --changed-use --update --deep "${TOOLCHAIN_PKGS[@]}"
+    unset btt_emerge
 }
 
 configure_crossdev_overlay / /usr/local/portage/crossdev
