8d9e17bc8b18a43b260211f190aba929e6b2361a
Sayan Chowdhury <schowdhury@microsoft.com>
2025-06-20 14:18:36 +0530

    Publish AZ SIG images
    Signed-off-by: Sayan Chowdhury <schowdhury@microsoft.com>

---
+++ ci-automation/az_sig_publish.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+function az_sig_publish() {
+  # Run a subshell, so the traps, environment changes and global
+  # variables are not spilled into the caller.
+  (
+      set -euo pipefail
+
+      _az_sig_publish_impl "${@}"
+  )
+}
+
+# --
+function _az_sig_publish_impl() {
+  local arch="$1"
+  local push_non_nightly_builds="${PUSH_NON_NIGHTLY_BUILDS:-}"
+
+  source sdk_lib/sdk_container_common.sh
+  local channel=""
+  channel="$(get_git_channel)"
+
+  source sdk_container/.repo/manifests/version.txt
+  local vernum="${FLATCAR_VERSION}"
+
+  source ci-automation/ci_automation_common.sh
+  source ci-automation/gpg_setup.sh
+
+  if [[ "$vernum" != *nightly* && "${push_non_nightly_builds}" != "true" ]]; then
+    echo "INFO: Version '$vernum' is not a nightly build, and PUSH_NON_NIGHTLY_BUILDS is not enabled. Skipping publish step."
+    exit 0
+  fi
+
+  #azure_auth_config_file=""
+  #secret_to_file azure_auth_config_file "${AZURE_AUTH_CREDENTIALS}"
+  #export AZURE_CLIENT_ID=$(jq -r '.clientId' "${azure_auth_config_file}")
+  #export AZURE_CLIENT_SECRET=$(jq -r '.clientSecret' "${azure_auth_config_file}")
+  #export AZURE_TENANT_ID=$(jq -r '.tenantId' "${azure_auth_config_file}")
+
+  docker run --pull always --rm --net host \
+    --env AZURE_CLIENT_ID \
+    --env AZURE_CLIENT_SECRET \
+    --env AZURE_TENANT_ID \
+    --env VHD_STORAGE_ACCOUNT_NAME \
+    --env AZURE_LOCATION \
+    --env PUBLISHING_SIG_RESOURCE_GROUP \
+    --env STAGING_SIG_RESOURCE_GROUP \
+    --env FLATCAR_STAGING_GALLERY_NAME \
+    --env FLATCAR_GALLERY_NAME \
+    --env FLATCAR_ARCH="${arch}" \
+    --env FLATCAR_VERSION \
+    --env FLATCAR_CHANNEL \
+    -v "$PWD":/work \
+    -w /work \
+    mcr.microsoft.com/azure-cli \
+    /work/az_sig_publish
+}
+# --
