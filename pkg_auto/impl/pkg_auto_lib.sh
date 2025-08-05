429dd8941f2d16928e66c041538473082514accd
Krzesimir Nowak <knowak@microsoft.com>
2025-08-05 19:00:24 +0200

    Revert "debug"
    This reverts commit 238f349a9a531992aead5576d7e334fc6e7b50aa.

--- pkg_auto/impl/pkg_auto_lib.sh
+++ pkg_auto/impl/pkg_auto_lib.sh
@@ -3276,7 +3276,6 @@ function evaluate_licenses() {
         unset -n kv_map_ref
         local -n use_flags_array_ref=${use_flags_array_name}
 
-        set -x
         el_use_flags_map=()
         for use in "${use_flags_array_ref[@]}"; do
             use=${use//'('/}
@@ -3294,7 +3293,6 @@ function evaluate_licenses() {
         done
 
         evaluate_license_group "${license_group_name}" el_use_flags_map el_used_licenses
-        set +x
 
         unset -n use_flags_array_ref
     done
