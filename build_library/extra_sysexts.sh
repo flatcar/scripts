42917da21ea2723014e113f4120246433bef90ae
Krzesimir Nowak <knowak@microsoft.com>
2025-10-08 12:19:38 +0200

    build_library/extra_sysexts.sh: Sort entries
    Signed-off-by: Krzesimir Nowak <knowak@microsoft.com>

--- build_library/extra_sysexts.sh
+++ build_library/extra_sysexts.sh
@@ -1,5 +1,4 @@
 EXTRA_SYSEXTS=(
-  "overlaybd|sys-fs/overlaybd,app-containers/accelerated-container-image"
   "incus|app-containers/incus"
   "nvidia-drivers-535|x11-drivers/nvidia-drivers:0/535|-kernel-open persistenced|amd64"
   "nvidia-drivers-535-open|x11-drivers/nvidia-drivers:0/535|kernel-open persistenced|amd64"
@@ -7,6 +6,7 @@ EXTRA_SYSEXTS=(
   "nvidia-drivers-550-open|x11-drivers/old-nvidia-drivers:0/550|kernel-open persistenced|amd64"
   "nvidia-drivers-570|x11-drivers/nvidia-drivers:0/570|-kernel-open persistenced|amd64"
   "nvidia-drivers-570-open|x11-drivers/nvidia-drivers:0/570|kernel-open persistenced|amd64"
+  "overlaybd|sys-fs/overlaybd,app-containers/accelerated-container-image"
   "podman|app-containers/podman,net-misc/passt"
   "python|dev-lang/python,dev-python/pip"
   "zfs|sys-fs/zfs"
