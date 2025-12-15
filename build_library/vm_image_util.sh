624abb2949e00f3ef83a18ff710e158182334be9
Daniel <daniel.zatovic@gmail.com>
2025-12-15 17:21:11 +0100

    Revert "sysext: Add OS-dependent sysext compression"
    This reverts commit 5686f7bc81eeb1a3db1c88f044f5252d5ccad57f.

--- build_library/vm_image_util.sh
+++ build_library/vm_image_util.sh
@@ -585,15 +585,11 @@ install_oem_sysext() {
     # important - it sets the name of a rootfs directory, which is
     # used to determine the package target in
     # coreos/base/profile.bashrc
-    #
-    # OEM sysexts are stored in the compressed partition, so we disable
-    # compression to avoid double-compression.
     local build_sysext_flags=(
         --board="${BOARD}"
         --squashfs_base="${VM_SRC_SYSEXT_IMG}"
         --image_builddir="${built_sysext_dir}"
         --metapkgs="${metapkg}"
-        --compression=none
         --install_root_basename="${VM_IMG_TYPE}-oem-sysext-rootfs"
     )
     local overlay_path mangle_fs
