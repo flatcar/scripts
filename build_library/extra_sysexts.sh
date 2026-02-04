5d48557f791a1dff38c16e0f91b1b1ea18174e61
James Le Cuirot <jlecuirot@microsoft.com>
2026-02-04 14:29:31 +0000

    build_packages: Exclude OEM sysext packages when running eclean packages
    Also simplify the exclusion mechanism while we're at it.
    
    Signed-off-by: James Le Cuirot <jlecuirot@microsoft.com>

--- build_library/extra_sysexts.sh
+++ build_library/extra_sysexts.sh
@@ -11,19 +11,3 @@ EXTRA_SYSEXTS=(
   "python|dev-lang/python,dev-python/pip"
   "zfs|sys-fs/zfs"
 )
-
-_get_unversioned_sysext_packages_unsorted() {
-  for sysext in "${EXTRA_SYSEXTS[@]}"; do
-    IFS="|" read -r _ PACKAGE_ATOMS _ <<< "$sysext"
-
-    IFS=,
-    for atom in $PACKAGE_ATOMS; do
-       qatom "$atom" -F "%{CATEGORY}/%{PN}"
-    done
-    unset IFS
-  done
-}
-
-get_unversioned_sysext_packages() {
-  _get_unversioned_sysext_packages_unsorted | sort | uniq
-}
